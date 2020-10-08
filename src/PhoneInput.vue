0f27aeb0befd17b4af26f304590d072f232df423
mihailomat <mihailo.matijevic@homegate.ch>
2020-10-08 13:25:03 +0200

    add root component with serving option.

---
+++ src/PhoneInput.vue
@@ -0,0 +1,17 @@
+<template>
+  <label>
+    <p>{{ label }}</p>
+    <input
+  /></label>
+</template>
+<script>
+export default {
+  name: 'PhoneInput',
+  props: {
+    label: {
+      type: String,
+      default: 'phone',
+    },
+  },
+};
+</script>
