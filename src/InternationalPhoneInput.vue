4868bc845d46d54a260e7f8234745e66bf526677
mihailomat <mihailo.matijevic@homegate.ch>
2021-02-06 21:46:10 +0100

    Add binded value to the input.

--- src/InternationalPhoneInput.vue
+++ src/InternationalPhoneInput.vue
@@ -1,16 +1,34 @@
 <template>
   <label>
     <p>{{ label }}</p>
-    <input
-  /></label>
+    <input type="tel" v-model="internalModel" :placeholder="placeholder" />
+  </label>
 </template>
 <script>
 export default {
   name: 'InternationalPhoneInput',
   props: {
+    value: {
+      type: String,
+      default: '',
+    },
     label: {
       type: String,
-      default: 'phone',
+      default: '',
+    },
+    placeholder: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      internalModel: this.value,
+    };
+  },
+  watch: {
+    internalModel(newValue) {
+      this.$emit('input', newValue);
     },
   },
 };
