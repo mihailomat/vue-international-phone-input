5a6ae1b6c206729c246beaaab93e08bd73ee5734
mihailomat <mihailo.matijevic@homegate.ch>
2021-02-07 00:12:50 +0100

    Add example app for local test usage.

---
+++ src/ExamplesApp.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <InternationalPhoneInput
+      label="Phone number"
+      v-model="phoneNumber"
+      placeholder="enter phone number"
+    />
+    <p>Phone number: {{ phoneNumber }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import InternationalPhoneInput from './InternationalPhoneInput.vue';
+export default {
+  components: {
+    InternationalPhoneInput,
+  },
+  data: () => ({
+    phoneNumber: '',
+  }),
+};
+</script>
